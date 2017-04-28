9336abfe284d002f3ab198ad63e4d68100e899e6
NicholasWalter <NicholasWalter@web.de>
2017-04-28 15:27:37 +1200

    initial version commit

---
+++ README.md
@@ -0,0 +1,17 @@
+Very tiny python project that forces screen to remain turned on by sending control key inputs at specified intervals.
+
+Usage:
+bash:
+
+chmod +x caffeine.sh
+./caffeine.sh [interval]
+
+python:
+
+python3.x caffeine.py [interval]
+
+If no interval is specified, will send a key every 60 seconds, default interval can be changed in source code.
+
+
+
+//I know there are more elegant solutions but I needed this quickly and it works.
---
+++ caffeine.py
@@ -0,0 +1,42 @@
+import datetime
+import pyautogui
+import sys
+import time
+
+def wide_awake(seconds):
+    try:
+        print("Starting to keep awake. Use ctrl + C to exit.")
+        print("Simulating ctrl key press every {} seconds".format(seconds))
+        print("Start time: " + str(datetime.datetime.now()))
+        #pyautogui.MoveTo(0, 0)
+        while True:
+            time.sleep(seconds)
+            pyautogui.hotkey("ctrl")
+            #pyautogui.moveRel(1, 1)
+            time.sleep(seconds)
+            pyautogui.hotkey("ctrl")
+            #pyautogui.moveRel(-1, -1)
+    except KeyboardInterrupt:
+        print("User terminated program")
+        print("End time: " + str(datetime.datetime.now( )))
+        sys.exit()
+
+def print_usage():
+    print("Usage:")
+    print("python caffeine.py [seconds]")
+    print("seconds: time to elapse between movement of mouse cursor")
+
+if len(sys.argv) == 1:
+    seconds = 60
+elif len(sys.argv) == 2:
+    try:
+        seconds = int(sys.argv[1])
+    except:
+        print("Use integer as argument\n\n")
+        print_usage()
+        sys.exit()
+elif len(sys.argv) > 2:
+    print_usage()
+    sys.exit()
+
+wide_awake(seconds)
